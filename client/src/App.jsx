96ac78f820e29f2839687cceced23f689548697a
NikolVasileva <nikol.vasileva9@gmail.com>
2025-11-16 22:38:09 +0200

    Add Footer Component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,16 +2,20 @@ import { Route, Routes } from "react-router"
 import Header from "./components/header/Header.jsx"
 import Home from "./components/home/Home.jsx"
 import Recipes from "./components/Recipes/Recipes.jsx"
+import Footer from "./components/footer/Footer.jsx"
 
 function App() {
 
   return (
     <>
       <Header />
+
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/recipes" element={<Recipes />} />
       </Routes>
+
+      <Footer />
     </>
   )
 }
