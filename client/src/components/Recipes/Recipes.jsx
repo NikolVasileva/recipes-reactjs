f5656b8a78f73c1ae8adeadf8e1bd672bf16f7e3
NikolVasileva <nikol.vasileva9@gmail.com>
2025-11-29 22:07:07 +0200

    update json data and show dynamic info

--- client/src/components/Recipes/Recipes.jsx
+++ client/src/components/Recipes/Recipes.jsx
@@ -1,24 +1,29 @@
+import { useEffect, useState } from "react"
+import RecipeCard from "../recipes-card/RecipeCard.jsx"
+
 export default function Recipes() {
+
+    const[recipes, setRecipes] = useState([])
+
+    useEffect(() => {
+        fetch("http://localhost:3030/data/recipes")
+        .then(response => response.json())
+        .then(data => {
+            setRecipes(Object.values(data))
+        })
+        .catch(err => alert(err.message))
+    }, [])
+
     return(
         <div className="container-fluid service py-6">
             <div className="container">
                 <div className="text-center wow bounceInUp" data-wow-delay="0.1s">
-                    <small className="d-inline-block fw-bold text-dark text-uppercase bg-light border border-primary rounded-pill px-4 py-1 mb-3">Our Services</small>
-                    <h1 className="display-5 mb-5">What We Offer</h1>
+                    <small className="d-inline-block fw-bold text-dark text-uppercase bg-light border border-primary rounded-pill px-4 py-1 mb-3">Our ervices</small>
+                    <h1 className="display-5 mb-5">Choose recipe</h1>
                 </div>
                 <div className="row g-4">
-                    <div className="col-lg-3 col-md-6 col-sm-12 wow bounceInUp" data-wow-delay="0.1s">
-                        <div className="bg-light rounded service-item">
-                            <div className="service-content d-flex align-items-center justify-content-center p-4">
-                                <div className="service-content-icon text-center">
-                                <img src="img/hero.png" className="img-fluid rounded animated zoomIn" alt="Main image" />
-                                    <h4 className="mb-3">Wedding Services</h4>
-                                    <p className="mb-4">Contrary to popular belief, ipsum is not simply random.</p>
-                                    <a href="#" className="btn btn-primary px-4 py-2 rounded-pill">Read More</a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+                    {recipes.length === 0 && <h3>Sorry, no added recipes yet!</h3>}
+                    {recipes.map(recipe => <RecipeCard key={recipe._id} {...recipe}/>)}
                 </div>
             </div>
         </div>
--- client/src/components/recipes-card/RecipeCard.jsx
+++ client/src/components/recipes-card/RecipeCard.jsx
@@ -1,11 +1,16 @@
-export default function RecipesCard() {
+export default function RecipesCard({
+    _id,
+    title,
+    imageUrl,
+
+}) {
     return(
         <div className="col-lg-3 col-md-6 col-sm-12 wow bounceInUp" data-wow-delay="0.1s">
         <div className="bg-light rounded service-item">
             <div className="service-content d-flex align-items-center justify-content-center p-4">
                 <div className="service-content-icon text-center">
-                <img src="img/hero.png" className="img-fluid rounded animated zoomIn" alt="Main image" />
-                    <h4 className="mb-3">Wedding Services</h4>
+                <img src={imageUrl} className="img-fluid rounded animated zoomIn" alt="Main image" />
+                    <h4 className="mb-3">{title}</h4>
                     <p className="mb-4">Contrary to popular belief, ipsum is not simply random.</p>
                     <a href="#" className="btn btn-primary px-4 py-2 rounded-pill">Read More</a>
                 </div>
