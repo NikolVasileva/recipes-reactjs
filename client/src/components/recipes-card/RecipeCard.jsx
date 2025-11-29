385cea6f4faa037dad06396c4f9fde045f7ab00d
NikolVasileva <nikol.vasileva9@gmail.com>
2025-11-29 22:53:13 +0200

    update recipes card elements

--- client/src/components/recipes-card/RecipeCard.jsx
+++ client/src/components/recipes-card/RecipeCard.jsx
@@ -2,20 +2,40 @@ export default function RecipesCard({
     _id,
     title,
     imageUrl,
+    servings,
+    cookTime,
+    difficulty
+
 
 }) {
-    return(
+    return (
+
+
         <div className="col-lg-3 col-md-6 col-sm-12 wow bounceInUp" data-wow-delay="0.1s">
-        <div className="bg-light rounded service-item">
-            <div className="service-content d-flex align-items-center justify-content-center p-4">
-                <div className="service-content-icon text-center">
-                <img src={imageUrl} className="img-fluid rounded animated zoomIn" alt="Main image" />
-                    <h4 className="mb-3">{title}</h4>
-                    <p className="mb-4">Contrary to popular belief, ipsum is not simply random.</p>
-                    <a href="#" className="btn btn-primary px-4 py-2 rounded-pill">Read More</a>
+            <div className="bg-light rounded service-item">
+                <div className="service-content d-flex align-items-center justify-content-center p-4">
+                    <div className="service-content-icon text-center">
+                        <img src={imageUrl} className="img-fluid rounded animated zoomIn" alt={title} />
+                        <h4 className="mb-3">{title}</h4>
+                        <p className="mb-4">Contrary to popular belief, ipsum is not simply random.</p>
+
+                        <div className="d-flex align-items-center justify-content-center mb-3">
+                            <i className="fas fa-utensils text-primary me-2"></i>
+                            <span>{servings} servings</span>
+                        </div>
+                        <div className="d-flex align-items-center justify-content-center mb-3">
+                            <i class="fa fa-clock text-primary me-2"></i>
+                            <span>{cookTime} mins</span>
+                        </div>
+                        <div className="d-flex align-items-center justify-content-center mb-3">
+                            <i className="fas fa-star text-primary me-2"></i>
+                            <span>{difficulty}</span>
+                        </div>
+
+                        <a href="#" className="btn btn-primary px-4 py-2 rounded-pill">Read More</a>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
     )
 }
