54e8fcb75b56924c0a629aee01a01b49fb56f999
NikolVasileva <nikol.vasileva9@gmail.com>
2025-12-02 19:54:48 +0200

    dynamic navigation

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -1,7 +1,10 @@
 import { Link } from "react-router";
+import { useUserContext } from "../../contexts/UserContext";
 
 export default function Header() {
-    return(
+    const { isAuthenticated } = useUserContext();
+
+    return (
         <div className="container-fluid nav-bar">
             <div className="container">
                 <nav className="navbar navbar-light navbar-expand-lg py-4">
@@ -15,11 +18,20 @@ export default function Header() {
                         <div className="navbar-nav mx-auto">
                             <Link to="/" className="nav-item nav-link active">Home</Link>
                             <Link to="/recipes" className="nav-item nav-link">Recipes</Link>
-                            <Link to="/recipes/create" className="nav-item nav-link">Add recipes</Link>
-                            <Link to="/register" className="nav-item nav-link">Register</Link>
-                            <Link to="/login" className="nav-item nav-link">Login</Link>
-                            <Link to="/logout" className="nav-item nav-link">Logout</Link>
-                            <a href="contact.html" className="nav-item nav-link">Contact</a>
+
+                            {isAuthenticated ? (
+                                <>
+                                    <Link to="/recipes/create" className="nav-item nav-link">Add recipes</Link>
+                                    <Link to="/logout" className="nav-item nav-link">Logout</Link>
+                                </>
+                            ) : (
+                                <>
+                                    <Link to="/register" className="nav-item nav-link">Register</Link>
+                                    <Link to="/login" className="nav-item nav-link">Login</Link>
+                                </>
+                            )}
+
+                            {/* <a href="contact.html" className="nav-item nav-link">Contact</a> */}
                         </div>
                         <button className="btn-search btn btn-primary btn-md-square me-4 rounded-circle d-none d-lg-inline-flex" data-bs-toggle="modal" data-bs-target="#searchModal"><i className="fas fa-search"></i></button>
                         <a href="" className="btn btn-primary py-2 px-4 d-none d-xl-inline-block rounded-pill">Book Now</a>
