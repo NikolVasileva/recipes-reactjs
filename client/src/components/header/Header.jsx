2c4fb053e4c23cba4a98ab789fbf1245452c4ed3
NikolVasileva <nikol.vasileva9@gmail.com>
2025-12-02 20:48:25 +0200

    Fix the logo link

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -8,7 +8,7 @@ export default function Header() {
         <div className="container-fluid nav-bar">
             <div className="container">
                 <nav className="navbar navbar-light navbar-expand-lg py-4">
-                    <Link href="/" className="navbar-brand">
+                    <Link to="/" className="navbar-brand">
                         <h1 className="text-primary fw-bold mb-0">Your<span className="text-dark">Taste</span> </h1>
                     </Link>
                     <button className="navbar-toggler py-2 px-3 collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-expanded="false">
