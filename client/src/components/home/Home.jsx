f412571148243e024a90aa7cc8829c383b029b26
NikolVasileva <nikol.vasileva9@gmail.com>
2025-12-03 21:00:04 +0200

    Add the newest recipes at home page

--- client/src/components/home/Home.jsx
+++ client/src/components/home/Home.jsx
@@ -1,4 +1,8 @@
+import useRequest from "../../hooks/useRequest"
+import RecipesCard from "../recipes-card/RecipeCard"
+
 export default function Home() {
+    const { data: latestRecipes } = useRequest("/data/recipes?sortBy=_createdOn%20desc&pageSize=4", [])
     return(
         <div className="container-fluid bg-light py-6 my-6 mt-0">
             <div className="container">
@@ -13,6 +17,18 @@ export default function Home() {
                     </div>
                 </div>
             </div>
+            <div className="container-fluid service py-6">
+            <div className="container">
+                <div className="text-center wow bounceInUp" data-wow-delay="0.1s">
+                    <small className="d-inline-block fw-bold text-dark text-uppercase bg-light border border-primary rounded-pill px-4 py-1 mb-3">Must try</small>
+                    <h1 className="display-5 mb-5">Explore the newest ones</h1>
+                </div>
+                <div className="row g-4">
+                    {latestRecipes.length === 0 && <h3>Sorry, no added recipes yet!</h3>}
+                    {latestRecipes.map(recipe => <RecipesCard key={recipe._id} {...recipe}/>)}
+                </div>
+            </div>
+        </div>
         </div>
     )
 }
