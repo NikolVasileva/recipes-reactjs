4ecd51c86005b8adbcb704de6b441f9ea0d6e8e3
NikolVasileva <nikol.vasileva9@gmail.com>
2025-12-06 19:24:45 +0200

    Add Local Storage logic

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,30 +8,31 @@ import Login from "./components/login/Login.jsx"
 import Register from "./components/register/Register.jsx"
 import RecipeCreate from "./components/recipe-create/RecipeCreate.jsx"
 import Edit from "./components/edit/Edit.jsx"
-import { useContext } from "react"
-import UserContext from "./contexts/UserContext.jsx"
+// import { useContext } from "react"
+// import UserContext from "./contexts/UserContext.jsx"
 import Logout from "./components/logout/Logout.jsx"
+import { UserProvider } from "./contexts/UserContext.jsx"
 
 
 function App() {
 
-    const {user} = useContext(UserContext)
+    // const {user} = useContext(UserContext)
 
   return (
-    <>
+    <UserProvider>
       <Header/>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/recipes" element={<Recipes />} />
         <Route path="/recipes/create" element={<RecipeCreate />} />
-        <Route path="/recipes/:recipeId/details" element={<Details user={user}/>} />
+        <Route path="/recipes/:recipeId/details" element={<Details/>} />
         <Route path="/recipes/:recipeId/edit" element={<Edit />} />
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
         <Route path="/logout" element={<Logout />} />
       </Routes>
       <Footer />
-    </>
+    </UserProvider>
   )
 }
 
