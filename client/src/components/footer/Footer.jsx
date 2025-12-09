76a3093e4a89c39c645774f09cab40ae1797ba27
NikolVasileva <nikol.vasileva9@gmail.com>
2025-12-09 19:46:31 +0200

    Update Footer Component

--- client/src/components/footer/Footer.jsx
+++ client/src/components/footer/Footer.jsx
@@ -1,14 +1,19 @@
+import { NavLink } from "react-router";
+
 export default function Footer() {
-    return(
-            <div className="container text-center justify-content-center align-items-center">
-                <div className="row justify-content-center">
-                    <div className="col-lg-6 col-md-10 text-center">
-                        <div className="footer-item text-center">
-                            <h1 className="text-primary">Your<span className="text-dark">Taste</span></h1>
-                            <p className="lh-lg mb-4">Explore YourTaste For Your Dream Dinner</p>
-                        </div>
+    return (
+        <div className="container text-center justify-content-center align-items-center">
+            <div className="row justify-content-center">
+                <div className="col-lg-6 col-md-10 text-center">
+                    <div className="footer-item text-center">
+                        <NavLink to="/">
+                            <h1 className="text-primary fw-bold mb-0">Your<span className="text-dark">Taste</span> </h1>
+                        </NavLink>
+                        <p className="lh-lg mb-4">Explore YourTaste For Your Dream Dinner</p>
+                        <p class="uppercase color-2 fs-text-16">© YourTaste 2025 - SoftUni ReactJS Project</p>
                     </div>
                 </div>
             </div>
+        </div>
     )
 }
